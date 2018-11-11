df326f692bb9baaaa56d54ad30aaad8b82b2563f
Anthony <a@sherrill.de>
2018-11-11 15:02:35 +0100

    added some menu-points and logic

--- menu.py
+++ menu.py
@@ -6,19 +6,25 @@ import os
 def menu():
     answer = 0
 
-    print "1. Sound-Senser-Version"
+    print "1. Sound-Sensor-Version"
     print "2. Szenario-Version"
-    print "3. Random input-Version\n"
+    print "3. Random input-Version"
+    print "4. Exit\n"
 
-    options = [1,2,3]
+    options = [1,2,3,4]
 
     while answer not in options:
-        answer = int(raw_input("Press 1, 2 or 3 and hit ENTER: "))
+        answer = int(raw_input("Press 1, 2, 3 or 4 and hit ENTER: "))
 
     if answer == 1:
-        os.system("python fading.py")
+        os.system("coware.py")
         menu()
     elif answer == 2:
+        os.system("coware.py")
+        print "2"
+        menu()
+    elif answer == 3:
+        os.system("fading.py")
         print "2"
         menu()
 
