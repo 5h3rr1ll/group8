b83a813f5ec8f10f88bfee8e8b55a2306b321a89
Anthony <a@sherrill.de>
2018-11-11 21:17:11 +0100

    test new scenario

--- soundsensor.py
+++ soundsensor.py
@@ -17,8 +17,8 @@ GPIO.setup(22,GPIO.OUT)
 GPIO.setup(24,GPIO.OUT)
 
 # define white light
-whiteOn =     GPIO.output(17,GPIO.HIGH), GPIO.output(22,GPIO.HIGH), GPIO.output(24,GPIO.HIGH)
-whiteOff = GPIO.output(17,GPIO.LOW), GPIO.output(22,GPIO.LOW), GPIO.output(24,GPIO.LOW)
+GPIO.output(17,GPIO.HIGH), GPIO.output(22,GPIO.HIGH), GPIO.output(24,GPIO.HIGH)
+
 # just turn white light on
 os.system("")
 
@@ -30,13 +30,21 @@ GPIO.setup(channel, GPIO.IN)
 
 def callback(channel):
 	if GPIO.input(channel):
-		print chanell
-		GPIO.output(17,GPIO.LOW)
-		GPIO.output(22,GPIO.HIGH)
-	else:
-		print "medium Sound Detected!2"
-		GPIO.output(22,GPIO.LOW)
-		GPIO.output(17,GPIO.HIGH)
+		print "It's gettign to noisy!"
+        # white light goes off
+        GPIO.output(17,GPIO.LOW), GPIO.output(22,GPIO.LOW), GPIO.output(24,GPIO.LOW)
+        #yellow light light up
+		os.system("pigs p 17 255")
+        os.system("pigs p 22 80")
+        time.sleep(5)
+        print("Way to loud!")
+        # turn yellow light off
+		os.system("pigs p 17 0")
+        os.system("pigs p 22 0")
+        for _ in range(5):
+                GPIO.output(22,GPIO.HIGH)
+                GPIO.output(22,GPIO.LOW)
+                return
 
 
 GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=100) # let us know when the pin goes High or LOW
